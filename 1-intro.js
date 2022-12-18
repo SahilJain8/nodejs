978e827780fef615d006346445a13ab2c4c2799f
SahilJain8 <jainsahil1116@gmail.com>
2022-12-18 12:38:48 +0530

    #

---
+++ 1-intro.js
@@ -0,0 +1,23 @@
+// const amount = 1
+
+// if (amount<10) {
+//     console.log(amount+"is less than 10");
+    
+// }
+// else{
+//     console.log(amount);
+// }
+
+
+console.log(__dirname);
+console.log(__filename)
+const sahil="sahil";
+
+// setInterval(() => {
+//     console.log('hello world')
+// },100)
+
+module.exports ={sahil};
+
+
+console.log(module)
---
+++ mode.js
@@ -0,0 +1,16 @@
+//writing a function in nodjs
+
+// const sayhi = (name) => {
+//     console.log('hello',name);
+
+
+
+// }
+
+const name = require('./1-intro.js')
+
+
+// sayhi('sahil')
+
+console.log(name.sahil)
+
